11851ea57cae7f290f65d131fa41463ac415883f
Elie Andraos <elieandraos31@gmail.com>
2022-07-28 14:42:24 +0300

    :bricks: setup electron

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import * as path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: path.resolve(__dirname, "./dist/"),
   plugins: [vue()],
   resolve: {
     alias: {
