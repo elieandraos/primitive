926721c205d2f08ff0ffc442335e7261b249aef7
Elie Andraos <elieandraos31@gmail.com>
2022-08-09 18:45:01 +0300

    :alembic: run npm install before executing

--- bin/primitive.js
+++ bin/primitive.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 const {exec, cd } = require('shelljs');
 
-const dir = process.argv.length === 3 && process.argv[2] === '--local' ? 'primitive' : '@elieandraos/primitive'
+const dir = '@elieandraos/primitive'
 const { stdout } = exec('npm root -g', { silent: true })
 const root = stdout.trim() + '/' + dir
 
 cd(root)
-exec('npm run electron:start')
+exec('npm i && npm run electron:start')
