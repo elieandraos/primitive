486b37af41eebefaf01e37804c16286d198976f8
Elie Andraos <elieandraos31@gmail.com>
2022-08-20 22:37:43 +0300

    :bricks: filesystem functions and tests

---
+++ cli/filesystem.js
@@ -0,0 +1,68 @@
+const fs  = require('fs')
+const path = require('path')
+
+const exists = (src) => {
+    return fs.existsSync(src)
+}
+
+const isDirectory = (src) => {
+    if (!exists(src)) return false
+
+    const stat = fs.statSync(src)
+    return stat.isDirectory()
+}
+
+const isFile = (src) => {
+    if (!exists(src)) return false
+
+    const stat = fs.statSync(src)
+    return stat.isFile()
+}
+
+const createDirectory = (dir, options = {}) => {
+    fs.mkdirSync(dir, options)
+}
+
+const emptyDirectory = (src) => {
+    if (!isDirectory(src)) throw `directory ${src} does not exist`
+
+    for (const file of fs.readdirSync(src)) {
+        const abs = path.resolve(src, file)
+
+        if (fs.lstatSync(abs).isDirectory()) {
+            emptyDirectory(abs)
+            fs.rmdirSync(abs)
+        } else {
+            fs.unlinkSync(abs)
+        }
+    }
+}
+
+const copy = (src, dest) => {
+    if (!exists(src)) throw `No such file or directory ${src}`
+
+    isDirectory(src) ? copyDir(src, dest) : copyFile(src, dest)
+}
+
+const copyFile = (src, dest) => {
+    fs.copyFileSync(src, dest)
+}
+
+const copyDir = (src, dest) => {
+    createDirectory(dest, { recursive: true })
+
+    for (const file of fs.readdirSync(src)) {
+        const srcFile = path.resolve(src, file)
+        const destFile = path.resolve(dest, file)
+        copy(srcFile, destFile)
+    }
+}
+
+module.exports = {
+    exists,
+    isDirectory,
+    isFile,
+    createDirectory,
+    emptyDirectory,
+    copy
+}
--- tests/release.test.js
+++ tests/release.test.js
@@ -1,12 +1,12 @@
 const _release = require('./../cli/release')
 
 describe('release pre-requisites check', () => {
-    test('it passes a valid github token', async () => {
+    test('it passes with a valid github token', async () => {
         const isValid = await _release.validateGithubToken(process.env.GITHUB_PERSONAL_ACCESS_TOKEN)
         expect(isValid).toBe(true)
     })
 
-    test('it fails with invalid github token', async () => {
+    test('it fails with an invalid github token', async () => {
         const isValid = await _release.validateGithubToken('foo')
         expect(isValid).toBe(false)
     })
