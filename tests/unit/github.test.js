ec7bf67bbd4840d7dfc1955aafccf3eb66fa8d33
Elie Andraos <elieandraos31@gmail.com>
2022-11-03 12:47:37 +0200

    :bug: access env variables

--- tests/unit/github.test.js
+++ tests/unit/github.test.js
@@ -1,8 +1,9 @@
+require("dotenv").config()
 const { validateGithubAccessToken } = require("../../cli/utils/github");
 
 describe('Github unit test suite', () => {
     test('it validates the personal access token', async () => {
-        const valid = await validateGithubAccessToken(process.env.GITHUB_PERSONAL_ACCESS_TOKEN)
+        const valid = await validateGithubAccessToken(process.env.GITHUB_TEST_TOKEN)
         expect(valid).toBe(true)
 
         const invalid = await validateGithubAccessToken('foo')
